72877787fb997ae9505ceb153baed5b624a71aa5
wn170298 <wali.noorzad12@gmail.com>
2025-12-04 11:18:09 +0100

    Create expenses.js

---
+++ api/expenses.js
@@ -0,0 +1,10 @@
+// ... (All other expense and helper code)
+
+/**
+ * Main handler for the Vercel Serverless Function.
+ * @param {import('http').IncomingMessage} req
+ * @param {import('http').ServerResponse} res
+ */
+export default async function handler(req, res) {
+    // ... (Function logic)
+}
